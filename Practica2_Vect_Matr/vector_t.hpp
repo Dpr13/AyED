2800106a8d9a68649ef873b1342c25e228e928b3
Daniel Pérez <110666246+Dpr13@users.noreply.github.com>
2023-03-02 20:53:04 +0000

    Update vector_t.hpp

--- Practica2_Vect_Matr/vector_t.hpp
+++ Practica2_Vect_Matr/vector_t.hpp
@@ -4,9 +4,6 @@
 // VERSION: 1.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 2
-// COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
-//              "C++ Programming Style Guidelines"
-//              https://geosoft.no/development/cppstyle.html
 
 #pragma once
 
@@ -15,53 +12,49 @@
 
 using namespace std;
 
+// Clase vector_t
+// Contiene vector y su medida
 template<class T> class vector_t {
  public:
+  // Constructores
   vector_t(const int = 0);
   ~vector_t();
-  
+  // Cambiar la medida del vector 
   void resize(const int);
-  
-  // getters
-  T get_val(const int) const;
-  int get_size(void) const;
-  
-  // setters
-  void set_val(const int, const T);
-  
-  // getters-setters
-  T& at(const int);
-  T& operator[](const int);
-  
-  // getters constantes
-  const T& at(const int) const;
-  const T& operator[](const int) const;
-
-  void write(ostream& = cout) const;
-  void read(istream& = cin);
+  // Getters
+  T get_val(const int) const; // Obtiene el valor del elemento seleccionado
+  int get_size(void) const; // Obtiene la medida del vector
+  // Setters
+  void set_val(const int, const T); // Cambia el valor del elemento seleccionado
+  // Getters-Setters
+  T& at(const int); // Retorna el valor del elemento seleccionado
+  T& operator[](const int); // Retorna el valor del elemento seleccionado
+  // Getters constantes
+  const T& at(const int) const; // Retorna el valor del elemento seleccionado
+  const T& operator[](const int) const; // Retorna el valor del elemento seleccionado
+  void write(ostream& = cout) const; // Imprime el vector
+  void read(istream& = cin); // Redefine el vector
 
  private:
   T *v_;
   int sz_;
   
-  void build(void);
-  void destroy(void);
+  void build(void); // Creación del vector
+  void destroy(void); // Destrucción del vector
 };
 
-
-
-template<class T> vector_t<T>::vector_t(const int n) { sz_ = n;
+// Constructor dándose la medida
+template<class T> vector_t<T>::vector_t(const int n) { 
+  sz_ = n;
   build();
 }
 
-
-
+// Destructor del vector
 template<class T> vector_t<T>::~vector_t() {
   destroy();
 }
 
-
-
+// Creación del vector
 template<class T> void vector_t<T>::build() {
   v_ = NULL;
   if (sz_ != 0) {
@@ -70,8 +63,7 @@ template<class T> void vector_t<T>::build() {
   }
 }
 
-
-
+//  Destrucción del vector
 template<class T> void vector_t<T>::destroy() {
   if (v_ != NULL) {
     delete[] v_;
@@ -80,62 +72,53 @@ template<class T> void vector_t<T>::destroy() {
   sz_ = 0;
 }
 
-
-
+// Redefinición de la medida del vector
 template<class T> void vector_t<T>::resize(const int n) {
-  destroy();
+  destroy();    // Se destruye primero el vector
   sz_ = n;
-  build();
+  build();      // Se crea de nuevo con la nueva medida
 }
 
-
-
+// Obtención del valor de un elemento del vector
 template<class T> inline T vector_t<T>::get_val(const int i) const {
   assert(i >= 0 && i < get_size());
   return v_[i];
 }
 
-
-
+// Obtención de la medida de un elemento del vector
 template<class T> inline int vector_t<T>::get_size() const {
   return sz_;
 }
 
-
-
+// Se actualiza el valor seleccionado
 template<class T> void vector_t<T>::set_val(const int i, const T d) {
   assert(i >= 0 && i < get_size());
   v_[i] = d;
 }
 
-
-
+// Se actualiza el valor del elemento
 template<class T> T& vector_t<T>::at(const int i) {
   assert(i >= 0 && i < get_size());
   return v_[i];
 }
 
-
-
+// Se actualiza el valor del elemento
 template<class T> T& vector_t<T>::operator[](const int i) {
   return at(i);
 }
 
-
-
+// Se actualiza el valor del elemento
 template<class T> const T& vector_t<T>::at(const int i) const {
   assert(i >= 0 && i < get_size());
   return v_[i];
 }
 
-
-
+// Se actualiza el valor del elemento
 template<class T> const T& vector_t<T>::operator[](const int i) const {
   return at(i);
 }
 
-
-
+// Se imprime el vector
 template<class T> void vector_t<T>::write(ostream& os) const { 
   os << get_size() << ":\t";
   for (int i = 0; i < get_size(); i++)
@@ -143,8 +126,7 @@ template<class T> void vector_t<T>::write(ostream& os) const {
   os << endl;
 }
 
-
-
+// Se modifica el vector aportándose los valores
 template<class T> void vector_t<T>::read(istream& is) {
   is >> sz_;
   resize(sz_);
@@ -154,8 +136,10 @@ template<class T> void vector_t<T>::read(istream& is) {
 
 
 // FASE II: producto escalar
+// Producto escalar de dos vectores
+// Parámetros: dos vectores a usar
 template<class T> T scal_prod(const vector_t<T>& v, const vector_t<T>& w) {
-  assert(v.get_size() == w.get_size());
+  assert(v.get_size() == w.get_size()); // Comprobamos que las medidas de ambos vectores son iguales
   double producto_escalar{0};
   for (int i{0}; i < v.get_size(); ++i) {
     producto_escalar = producto_escalar + v[i] * w[i];
@@ -163,8 +147,10 @@ template<class T> T scal_prod(const vector_t<T>& v, const vector_t<T>& w) {
   return producto_escalar;
 }
 
+// Prodcuto escalar de dos vectpres
+// Parámetros: dos vectores a usar
 double scal_prod(const vector_t<rational_t>& v, const vector_t<rational_t>& w) {
-  assert(v.get_size() == w.get_size());
+  assert(v.get_size() == w.get_size()); // Comprobamos que las medidas de ambos vectores son iguales
   double producto_escalar{0};
   for (int i{0}; i < v.get_size(); ++i) {
     rational_t total{0,1};
