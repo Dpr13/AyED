a3741fc7ecd8e260cc7d1271797d2775ecd2538b
Daniel Pérez <110666246+Dpr13@users.noreply.github.com>
2023-03-02 20:51:49 +0000

    Update main_2p.cc

--- Practica2_Vect_Matr/main_2p.cc
+++ Practica2_Vect_Matr/main_2p.cc
@@ -4,66 +4,54 @@
 // VERSION: 1.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 2
-// COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
-//              "C++ Programming Style Guidelines"
-//              https://geosoft.no/development/cppstyle.html
-// COMPILACIÓN: g++ -g rational_t.cpp main_p2.cpp -o main_p2
-
-// pauta de estilo [92]: comentarios multilínea usando solo "//"
 
 #include <iostream>
 #include <cmath>
 
-// pauta de estilo [41]: ficheros de cabecera agrupados y separados
 #include "rational_t.hpp"
-
 #include "vector_t.hpp"
-
 #include "matrix_t.hpp"
 
 using namespace std;
 
-int main()
-{
+int main() {
+  // Inicializamos dos números racionales
   rational_t a(1, 2), b(3);
 
   // FASE I
+  // Se realizan ciertas operaciones con estos números, que se imprimen
   cout << "a + b: ";
   (a+b).write();
-
   cout << "b - a: ";
   (b-a).write();
-
   cout << "a * b: ";
   (a*b).write();
-  
   cout << "a / b: ";
   (a/b).write();
-  
   cout << endl;
   
   // FASE II
+  // Se inicializan dos vectores
   vector_t<double> v, w;
-  v.read(), v.write();
+  v.read(), v.write(); 
   w.read(), w.write();
-  
+  // Producto escalar de los vectores creados anteriormente
   cout << "Producto escalar de vector_t<double>: " << scal_prod(v, w) << endl << endl;
-  
+  // Se crea vector de números racionales
   vector_t<rational_t> x, y;
   x.read(), x.write();
   y.read(), y.write();
-  
+  // Producto escalar de los vectores de números racionales
   cout << "Producto escalar de vector_t<rational_t>: " << scal_prod(x, y) << endl << endl;
   
-  
   // FASE III
+  // Se crean tres matrices
   matrix_t<double> A, B, C;
   A.read(), A.write();
   B.read(), B.write();
-  
+  // Producto matricial de las dos primeras matrices queda en la matriz C
   C.multiply(A, B);
   cout << "Multiplicación de matrices A y B: " << endl;
-  C.write();
-  
+  C.write(); 
   return 0;
 }
